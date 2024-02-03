a3cd71a2ea5181fe72f37f69737c453325900238
angginmegantara <angginmegantara@gmail.com>
2024-02-03 12:37:00 +0700

    Update Patch v1.1

--- src/index.js
+++ src/index.js
@@ -5,20 +5,16 @@ import './assets/css/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotificationPage from './pages/NotificationPage';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route index element={<App />} />
-          <Route path="notification" element={<NotificationPage />} />
-          {/* <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
+          {/* <Route index element={<App />} /> */}
+          <Route path="/notification" element={<NotificationPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
